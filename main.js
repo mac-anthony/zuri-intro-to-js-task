32422f620fb3dc63ba2e68744469cbe43f216864
Okeke Mac-Anthony <ekenemacanthony@gmail.comgit config --global user.name mac-anthonygit config user.name mac_anthonygit config user.email ekenemacanthony@gmail.com>
2022-06-21 21:59:50 +0100

    committing all the files

---
+++ main.js
@@ -0,0 +1,8 @@
+let myName = 'Mac-anthony';
+
+let myHeight = 178;
+
+let myCountry = 'Nigeria'
+
+// alert(` Name: ${myName}, Height: ${myHeight},  Country: ${myCountry} `)
+document.write(` Name: ${myName}, Height: ${myHeight},  Country: ${myCountry} `)
